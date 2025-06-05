b5c1ccc4ead02c2c5364964a8bc4383a5f89604f
Erick Almeida <Erick2280@users.noreply.github.com>
2025-06-05 00:41:49 -0300

    feat: implement error page for 404

--- src/app/global/pages/error/error.component.ts
+++ src/app/global/pages/error/error.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-error',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './error.component.html',
 })
-export class ErrorPageComponent {}
+export class ErrorPageComponent implements OnInit, OnDestroy {
+  ErrorType = ErrorType;
+  errorType?: ErrorType;
+
+  route = inject(ActivatedRoute);
+  routeSubscription?: Subscription;
+
+  ngOnInit(): void {
+    this.route.data.subscribe((data) => {
+      this.errorType = data['errorType'];
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription?.unsubscribe();
+  }
+}
+
+export enum ErrorType {
+  NotFound = 'NotFound',
+}
