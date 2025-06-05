1c9a7528d06cc3ad1ef364dbaa5de927301bbfec
Erick Almeida <Erick2280@users.noreply.github.com>
2025-06-05 12:57:53 -0300

    feat: scaffold eight queens project page

--- src/app/projects/eight-queens/eight-queens.component.ts
+++ src/app/projects/eight-queens/eight-queens.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { remixPlayCircleFill } from '@ng-icons/remixicon';
 
 @Component({
   selector: 'app-eight-queens',
-  imports: [],
+  imports: [RouterModule, NgIcon],
   templateUrl: './eight-queens.component.html',
+  viewProviders: [provideIcons({ remixPlayCircleFill })],
 })
 export class EightQueensPageComponent {}
