55cff3ea6a4babc5ffbe28d8fab8f614cf7d409d
Erick Almeida <Erick2280@users.noreply.github.com>
2025-06-06 20:39:44 -0300

    feat: implement view transition animation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,20 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from '@app/app.routes';
+import { viewTransitionHandler } from '@app/view-transition-handler';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withViewTransitions({
+        onViewTransitionCreated: viewTransitionHandler,
+      }),
+    ),
   ],
 };
